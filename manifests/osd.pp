885c5379e29c4c8d6717ae5a1f3ac5316aec1fdd
Pierre-François Honoré <pierre-francois.honore@cea.fr>
2017-02-22 16:10:57 +0100

    bluestore

--- manifests/osd.pp
+++ manifests/osd.pp
@@ -169,7 +169,7 @@ if ! test -b ${data} ; then
         chown -h ceph:ceph ${data}
     fi
 fi
-if $objectstore = "bluestore" ; then
+if $objectstore == "bluestore" ; then
   ceph-disk prepare ${cluster_option} ${fsid_option} --bluestore ${data}
 else
   ceph-disk prepare ${cluster_option} ${fsid_option} ${data} ${journal}
